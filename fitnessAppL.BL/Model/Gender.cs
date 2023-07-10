3225b00ea42a6e5dcdae9e58ab708562ec8dcbe0
Kirill Lipatov <lipa911@list.ru>
2023-07-10 23:38:18 +0300

    Добавьте файлы проекта.

---
+++ fitnessAppL.BL/Model/Gender.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessAppL.BL.Model
+{
+    /// <summary>
+    /// Пол.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Имя пола.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать новый пол.
+        /// </summary>
+        /// <param name="name">Имя пола.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public Gender(string name) 
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Строка пола не может быть пустым или NULL", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ fitnessAppL.CMD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessAppL.CMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
